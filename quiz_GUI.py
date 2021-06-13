cb9bcb5fa0c1b9f4a78d01c81089fdc8420b9cff
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2021-06-14 09:55:45 +1200

    Update quiz_GUI.py

--- quiz_GUI.py
+++ quiz_GUI.py
@@ -4,14 +4,20 @@ import random
 
 
 class Quiz:
-    
-    # GUI Frame
+
+    def __init__(self):
+        
+        # GUI Frame
         self.quiz_frame = Frame(padx=10, pady=10)
         self.quiz_frame.grid()
-    # Question Label to show user what question they're on
-    self.error_label = Label(self.start_frame, text="",
-                                font="Arial 10 italic", wraplength=250, fg="red")
-        self.error_label.grid(row=1)
+
+        # Question Label to show user what question they're on (row 1)
+        self.question_label = Label(self.quiz_frame, text="{} out of 10", font="Arial 10 italic", wraplength=250)
+        self.question_label.grid(row=1)
+
+        # Label for generated question (row 2)
+        self.gen_ques_label = Label(self.quiz_frame, text="{} out of 10", font="Arial 10 italic", wraplength=250)
+        self.gen_ques_label.grid(row=2)
 
 
 
@@ -20,5 +26,5 @@ class Quiz:
 if __name__ == "__main__":
     root = Tk()
     root.title("Math Quiz")
-    something = Quiz(root)
+    something = Quiz()
     root.mainloop()
