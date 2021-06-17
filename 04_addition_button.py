1eb55f7041c2beba1b04d2349ed4cf2dc07151d3
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2021-06-17 12:12:23 +1200

    Create 04_addition_button.py

---
+++ 04_addition_button.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+import random
+
+
+class Start:
+        
+    def Addition(self):
+        
+
+        
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Math Quiz")
+    something = Start()
+    root.mainloop()
