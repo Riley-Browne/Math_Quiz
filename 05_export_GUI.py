152594eed51d6423b981ce0f895d5a0a2251993b
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2021-07-09 11:08:45 +1200

    Update 05_export_GUI.py

--- 05_export_GUI.py
+++ 05_export_GUI.py
@@ -25,10 +25,10 @@ class Export:
 
         # Save Button
         self.save_button_button = Button(self.export_frame, text="Save",
-                             font="Arial 10 bold", bg="#FF9933", ccommand=partial(lambda: self.save_history(partner, user_answer)))
+                             font="Arial 10 bold", bg="#FF9933", command=partial(lambda: self.save_history(answered_questions)))
         self.save_button_button.grid(row=3, column=0, padx=65, pady=10, sticky="ew")
 
-    def save_history(self, partner):
+    def save_history(self):
 
         # Regular expression to check filename is valid.
         valid_char = "[A-Za-z0-9_]"
